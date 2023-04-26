c2ed5927238c3f7e6e078a78d9094fabc54cbc4e
expelledboy <102334+expelledboy@users.noreply.github.com>
2023-04-26 15:33:51 +0200

    Add a more robust term handler in pre_encode

--- src/erlang_json_logger.erl
+++ src/erlang_json_logger.erl
@@ -32,12 +32,12 @@ format(Map = #{msg := {Format, Terms}}, Config) ->
 format_msg(Msg, Meta, Config) when
       is_map(Msg) andalso
       is_map(Meta) andalso
-      is_map(Config) ->
-    Defaults = #{ indent => 2 },
-    Config0 = maps:merge(Defaults, Config),
-    Msg0 = maps:merge(Msg, Meta),
+      is_list(Config) ->
+    Meta0 = maps:with([domain, file, label, level, line, logger_formatter, mfa, pid, report, time], Meta),
+    Msg0 = maps:merge(Msg, Meta0),
+    % io:format("Msg0: ~p~n", [Msg0]),
     Msg1 = map_nested(fun pre_encode/1, Msg0),
-    encode(Msg1, Config0).
+    encode(Msg1, Config).
 
 %% --
 
@@ -52,13 +52,53 @@ map_nested(Fun, Value) ->
 %% --
 
 encode(Map, Config) ->
-    jsx:encode(maps:to_list(Map), maps:to_list(Config)).
+    try
+        JSON = jsx:encode(Map, Config),
+        <<JSON/binary, "\n">>
+    catch
+        error:Error ->
+            io:format("jsx ~p ~p: ~p~n", [Error, Map, maps:to_list(Config)]),
+            throw(Error)
+    end.
 
-pre_encode(Pid) when is_pid(Pid) ->
-    list_to_binary(pid_to_list(Pid));
+pre_encode(T) when is_atom(T) ->
+    atom_to_binary(T, utf8);
+pre_encode(PidRef) when is_pid(PidRef) or is_reference(PidRef) ->
+    list_to_binary(pid_to_list(PidRef));
+pre_encode(Fun) when is_function(Fun) ->
+    <<"#function">>;
+pre_encode(Tuple) when is_tuple(Tuple) ->
+    list_to_binary(io_lib:format("~tp", [Tuple]));
+pre_encode(Map) when is_map(Map) ->
+    maps:from_list([
+        {Key, pre_encode(Value)} || {Key, Value} <- maps:to_list(Map)
+    ]);
+pre_encode(List) when is_list(List) ->
+    case io_lib:printable_list(List) of
+        true ->
+            case unicode:characters_to_binary(List) of
+                {_, _, _} -> % error or incomplete
+                    list_to_binary(io_lib:format("~tp", [List]));
+                Binary ->
+                    Binary
+            end;
+        false ->
+            case is_proplist(List) of
+                true ->
+                    pre_encode(maps:from_list(List));
+                false ->
+                    list_to_binary(io_lib:format("~tp", [List]))
+            end
+    end;
 pre_encode([{Pid1, Pid2} | Rest]) when is_pid(Pid1), is_pid(Pid2) ->
     [{pre_encode(Pid1), pre_encode(Pid2)} | pre_encode(Rest)];
 pre_encode([{Pid, Other} | Rest]) when is_pid(Pid) ->
     [{pre_encode(Pid), Other} | pre_encode(Rest)];
 pre_encode(Other) ->
     Other.
+
+%% --
+
+is_proplist([]) -> true;
+is_proplist([{K,_}|L]) when is_atom(K) -> is_proplist(L);
+is_proplist(_) -> false.
